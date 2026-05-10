83bcb7717bf9e56949dfd044b32beacef0694176
Astro - Arthur <astrochanneloficial@gmail.com>
2026-05-10 17:15:12 -0300

    Add langsinstall.sh for installing programming languages

---
+++ scripts/langsinstall.sh
@@ -0,0 +1,18 @@
+echo "Iniciando LangsInstall do WindowsAstroSkript..."
+
+echo "Instalando Python v3.13..."
+
+winget install -e --id Python.Python.3.12
+
+echo "Instalando GoLang..."
+
+winget install -e --id GoLang.Go
+
+echo "Instalando Java..."
+
+winget install Microsoft.OpenJDK.21
+
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo "Prontinho! As Linguagens estão instaladas! Rode o gamesinstall.sh para finalizar <3"
