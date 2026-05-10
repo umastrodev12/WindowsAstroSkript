760f64328c42147b8b1af07c270145ab5f7645b7
Astro - Arthur <astrochanneloficial@gmail.com>
2026-05-10 17:15:35 -0300

    Add gamesinstall.sh script for game installations

---
+++ scripts/gamesinstall.sh
@@ -0,0 +1,10 @@
+echo "Iniciando GamesInstall do WindowsAstroSkript..."
+
+echo "Instalando Roblox..."
+
+winget install -e --id Roblox.Roblox --exact
+
+echo "Instalando Lunar Client: Minecraft..."
+winget install -e --id Moonsworth.LunarClient # Lunar Client: Minecraft
+
+echo "Prontinho! Setup Finalizado!"
